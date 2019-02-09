6c78b7d49b6af4072fa7a21cafa768eb718fde5d
Mike Vercoelen <mike.vercoelen@gmail.com>
2019-02-09 16:18:21 +0100

    Clean up

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
 const WORKERS = process.env.WORKERS
-const LOG_LEVEL = process.env.LOG_LEVEL
 const JWT_LIFE_TIME = process.env.JWT_LIFE_TIME
 const JWT_SECRET = process.env.JWT_SECRET
 
@@ -9,7 +8,6 @@ module.exports = {
   PORT,
   MONGODB_URI,
   WORKERS,
-  LOG_LEVEL,
   JWT_LIFE_TIME,
   JWT_SECRET
 }
